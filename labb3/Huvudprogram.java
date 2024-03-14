5ce017a58fde22ee859d3e27497cf553177d9c81
LeoMan0 <leoman3628@gmail.com>
2024-03-14 18:37:35 +0100

    Done

--- labb3/Huvudprogram.java
+++ labb3/Huvudprogram.java
@@ -17,8 +17,8 @@ public class Huvudprogram {
 
     public static void main(String[] args) {
 
-        coolLevel();
-//        exampleLevel();
+        //coolLevel();
+        exampleLevel();
 
     }
 
